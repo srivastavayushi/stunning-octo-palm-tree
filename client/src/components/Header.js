3f8ee1e0c31cd7bc253731420da4bbc7356668f2
mansijain <mansi142002@gmail.com>
2021-06-05 16:29:19 +0530

    Updated files

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header class="text-center text-white text-5xl bg-blue-400 p-6">
+            <h1>Expense Tracker</h1>
+        </header>
+    )
+}
+
+export default Header
